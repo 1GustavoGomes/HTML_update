86ac2bf61cd41289702c831d842df41b8f57483f
1GustavoGomes <145237934+1GustavoGomes@users.noreply.github.com>
2024-10-24 11:48:37 -0300

    Add files via upload

---
+++ homepageJava.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const Easter_egg = document.getElementById("KiteIMG");
+    function easter_egg() {
+        alert("You found an Easter Egg! Congratulations :)");
+    }
+    Easter_egg.addEventListener("click", easter_egg);
+
+});
